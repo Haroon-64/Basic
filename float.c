d5a426294df88736756c28f903fc1408dd4c372a
haroon <haroonn475@gmail.com>
2022-09-21 18:34:46 +0530

    c1

--- float.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    
-    float real = 3.142592;
-    printf("%f",real);
-
-}
--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-//  MY CALCUALTOR USING C
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-// #include<conio.h>
