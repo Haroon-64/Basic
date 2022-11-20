d72d991ad8f1de16b176a64b64172ccb8c332938
haroon641 <76095593+Haroon-64@users.noreply.github.com>
2022-11-20 19:17:00 +0530

    Update Question(h).c

--- letuscsol/ch6/Question(h).c
+++ letuscsol/ch6/Question(h).c
@@ -5,7 +5,10 @@ int main()
 {
 	int i,j,k;
 	for(i=1;i<=30;i++)
-		for(j=1;j<=30;j++)
-			for(k=1;k<=30;k++)
-				printf("%d%d%d\n",i,j,k);
+		for(j=1;j<=30;j++)   //choose random 2 numbers then calculate the third triplet
+            for(k=1;k<=30;k++)
+               { if(i*i+j*j==k*k)
+                    printf("%d \t %d \t %d \n",i,j,k);
+			
+                }
 }
